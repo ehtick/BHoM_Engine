6c7d091d96b21d508e1363c250a6ad6146750071
Pawel Baran <pawel.baran@burohappold.com>
2025-09-11 16:14:17 +0200

    nurbsSurface.Reparametrise() added

--- Geometry_Engine/Modify/Reparametrise.cs
+++ Geometry_Engine/Modify/Reparametrise.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -20,7 +20,10 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using BH.oM.Base.Attributes;
 using BH.oM.Geometry;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace BH.Engine.Geometry
@@ -31,12 +34,58 @@ namespace BH.Engine.Geometry
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static NurbsCurve Reparatremise(this NurbsCurve curve)
+        [Description("Reparametrises a NurbsCurve to have a normalised domain from 0 to 1.")]
+        [Input("curve", "The NurbsCurve to reparametrise.")]
+        [Output("curve", "The reparametrised NurbsCurve with domain [0,1].")]
+        public static NurbsCurve Reparametrise(this NurbsCurve curve)
         {
             int degree = curve.Degree();
             double min = curve.Knots[degree - 1];
             double max = curve.Knots[curve.Knots.Count - degree];
             return new NurbsCurve { ControlPoints = curve.ControlPoints, Weights = curve.Weights, Knots = curve.Knots.Select(t => (t - min) / (max - min)).ToList() };
+        }
+
+        /***************************************************/
+
+        [Description("Reparametrises a NurbsSurface to have normalised U and V domains from 0 to 1.")]
+        [Input("surface", "The NurbsSurface to reparametrise.")]
+        [Output("surface", "The reparametrised NurbsSurface with U and V domains [0,1].")]
+        public static NurbsSurface Reparametrise(this NurbsSurface surface)
+        {
+            List<ICurve> innerTrims2D = surface.InnerTrims.Select(x => x.Curve2d).ToList();
+            List<ICurve> outerTrims2D = surface.OuterTrims.Select(x => x.Curve2d).ToList();
+
+            // Reparametrise U knots
+            double uMin = surface.UKnots[surface.UDegree - 1];
+            double uMax = surface.UKnots[surface.UKnots.Count - surface.UDegree];
+            double uTranslation = -uMin;
+            double uScale = 1 / (uMax - uMin);
+            List<double> reparametrisedUKnots = surface.UKnots.Select(u => (u + uTranslation) * uScale).ToList();
+            innerTrims2D = innerTrims2D.Select(x => x.ITranslate(new Vector { X = uTranslation }).IScale(Point.Origin, new Vector { X = uScale, Y = 1, Z = 1 })).ToList();
+            outerTrims2D = outerTrims2D.Select(x => x.ITranslate(new Vector { X = uTranslation }).IScale(Point.Origin, new Vector { X = uScale, Y = 1, Z = 1 })).ToList();
+
+            // Reparametrise V knots
+            double vMin = surface.VKnots[surface.VDegree - 1];
+            double vMax = surface.VKnots[surface.VKnots.Count - surface.VDegree];
+            double vTranslation = -vMin;
+            double vScale = 1 / (vMax - vMin);
+            List<double> reparametrisedVKnots = surface.VKnots.Select(v => (v + vTranslation) * vScale).ToList();
+            innerTrims2D = innerTrims2D.Select(x => x.ITranslate(new Vector { Y = vTranslation }).IScale(Point.Origin, new Vector { X = 1, Y = vScale, Z = 1 })).ToList();
+            outerTrims2D = outerTrims2D.Select(x => x.ITranslate(new Vector { Y = vTranslation }).IScale(Point.Origin, new Vector { X = 1, Y = vScale, Z = 1 })).ToList();
+
+            List<SurfaceTrim> innerTrims = surface.InnerTrims.Select((x, i) => new SurfaceTrim(x.Curve3d, innerTrims2D[i])).ToList();
+            List<SurfaceTrim> outerTrims = surface.OuterTrims.Select((x, i) => new SurfaceTrim(x.Curve3d, outerTrims2D[i])).ToList();
+
+            return new NurbsSurface(
+                surface.ControlPoints,
+                surface.Weights,
+                reparametrisedUKnots,
+                reparametrisedVKnots,
+                surface.UDegree,
+                surface.VDegree,
+                innerTrims,
+                outerTrims
+            );
         }
 
         /***************************************************/

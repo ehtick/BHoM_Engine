5566402127d527f824347298dede8d4294859121
Erik Narhi <Erik.Narhi@burohappold.com>
2025-12-11 09:12:42 -0800

    Versioning updated

--- Facade_Engine/Compute/UValueOpeningAW.cs
+++ Facade_Engine/Compute/UValueOpeningAW.cs
@@ -44,7 +44,7 @@ namespace BH.Engine.Facade
         /****          Public Methods                   ****/
         /***************************************************/
 
-        [PreviousVersion("8.3", "BH.Engine.Facade.OverallUValue(BH.oM.Facade.Elements.Opening)")]
+        [PreviousVersion("9.0", "BH.Engine.Facade.OverallUValue(BH.oM.Facade.Elements.Opening)")]
         [Description("Returns effective U-Value of opening calculated using the Area Weighting Method. Requires center of opening U-value, frame U-value and edge U-value as OpeningConstruction and FrameEdgeProperty fragments.")]
         [Input("opening", "Opening to find U-value for.")]
         [Input("isSpandrel", "Represents if the opening is a spandrel, in which case a larger edge width of 10 inches is used as per NFRC 100.")]
